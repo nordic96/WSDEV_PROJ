abb7b011f9ef37bd499cc8a28717b4c88d9b0869
DsGodh <jackasscankickyou@gmail.com>
2018-01-24 23:43:25 +0800

    Updates

--- App_Code/LogisticsService.cs
+++ App_Code/LogisticsService.cs
@@ -7,7 +7,7 @@ using System.Web.Services;
 /// <summary>
 /// Summary description for LogisticsService
 /// </summary>
-[WebService(Namespace = "http://tempuri.org/")]
+[WebService(Namespace = "http://www.tpifc.com/TPWebServices")]
 [WebServiceBinding(ConformsTo = WsiProfiles.BasicProfile1_1)]
 // To allow this Web Service to be called from script, using ASP.NET AJAX, uncomment the following line. 
 // [System.Web.Script.Services.ScriptService]
@@ -21,17 +21,7 @@ public class LogisticsService : System.Web.Services.WebService
         //InitializeComponent(); 
     }
 
-    [WebMethod]
-<<<<<<< HEAD
-    public string HelloWorld()
-    {
-        return "Hello World";
-    }
-=======
-    public string searchCountry(string countryCode, string countryName, string portCode, string portName)
-    {
-        return countryName;
-    }//end method
->>>>>>> a1cc6a4f5eea924c263f34bf9fd24f5e703e6d61
+    //[WebMethod]
+
 
 }
---
+++ CustomDuties.aspx.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using PortService;
+using System.Xml;
+using System.Data;
+
+public partial class CustomDuties : System.Web.UI.Page
+{
+    ExcelRead excel = new ExcelRead();
+    //ReadExcel excel = new ReadExcel();
+    airport cis = new airport();
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        gv1.DataSource = excel.ExcelReadData("https://www.customs.gov.sg/~/media/cus/files/business/valuation%20duties%20taxes%20and%20fees/list%20of%20dutiable%20goods20feb2017.xlsx?la=en");
+        gv1.DataBind();
+        ddlHRCode.DataSource = excel.ExcelReadData("https://www.customs.gov.sg/~/media/cus/files/business/valuation%20duties%20taxes%20and%20fees/list%20of%20dutiable%20goods20feb2017.xlsx?la=en");
+        ddlHRCode.DataBind();
+        //Label1.Text = excel.ReadExcelData(@"C:\Users\rhrlg\Downloads/portcode2012.xls");
+    }
+
+
+    protected void btnCalculate_Click(object sender, EventArgs e)
+    {
+        int hscode = 1;
+        int weight = Convert.ToInt32(tbWeight.Text);
+        string mc = ddlManufactured.Text;
+        int result;
+        if (mc == "Domestic")
+        {
+            result = 88 * weight;
+            lblTotPrice.Text = result.ToString();
+        }
+    }
+}
